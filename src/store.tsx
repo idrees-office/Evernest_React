9addc3d120152db8e2133b24bbd599e38dd9169d
Marsad1998 <marsadakbar1@gmail.com>
2024-12-20 15:54:43 +0500

    marsad merge

--- src/store.tsx
+++ src/store.tsx
@@ -6,6 +6,7 @@ import newsSlice from './slices/newsSlice';
 import DeveloperSlice from './slices/developerSlice';
 import AmenitiesSlice from './slices/amenitiesSlice';
 import LeadsSlice from './slices/dashboardSlice';
+
 const rootReducer = combineReducers({
     themeConfig: themeConfigSlice,
     auth: authSlice, // Combine auth directly here
