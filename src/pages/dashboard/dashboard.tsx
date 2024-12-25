e5add56faf4b1d9ee0c765a3100828ff6165d93b
idrees-office <idreesoffice9012@gmail.com>
2024-12-25 11:06:42 +0400

    updatecode

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -2,11 +2,8 @@ import { useState, useEffect, useRef } from 'react';
 import { Disclosure } from '@headlessui/react';
 import Tippy from '@tippyjs/react';
 import 'tippy.js/dist/tippy.css';
-import ReactQuill from 'react-quill';
 import 'react-quill/dist/quill.snow.css';
-import Dropdown from '../../components/Dropdown';
 import Swal from 'sweetalert2';
-import AnimateHeight from 'react-animate-height';
 import PerfectScrollbar from 'react-perfect-scrollbar';
 import { useDispatch, useSelector } from 'react-redux';
 import { IRootState, AppDispatch } from '../../store';
@@ -15,8 +12,6 @@ import IconMail from '../../components/Icon/IconMail';
 import IconCaretDown from '../../components/Icon/IconCaretDown';
 import IconBookmark from '../../components/Icon/IconBookmark';
 import IconVideo from '../../components/Icon/IconVideo';
-import IconUserPlus from '../../components/Icon/IconUserPlus';
-import IconPlus from '../../components/Icon/IconPlus';
 import IconRefresh from '../../components/Icon/IconRefresh';
 import IconMenu from '../../components/Icon/IconMenu';
 import IconSearch from '../../components/Icon/IconSearch';
@@ -26,8 +21,6 @@ import IconPrinter from '../../components/Icon/IconPrinter';
 import { topBarStatus, SidebarStatus, MatchColorList, DropdownOption } from '../../services/status';
 import { Leadslist } from '../../slices/dashboardSlice';
 import IconPhone from '../../components/Icon/IconPhone';
-import { Link } from 'react-router-dom';
-import IconCalendar from '../../components/Icon/IconCalendar';
 import Select from 'react-select';
 import { updateSingleLead } from '../../slices/dashboardSlice';
 import Toast from '../../services/toast';
@@ -49,23 +42,9 @@ const DashboardBox = () => {
     const [errors, setErrors] = useState<Record<string, string[]>>({});
     const [selectedTab, setSelectedTab] = useState<any>();
 
-    const defaultParams = {
-        id: null,
-        from: 'vristo@mail.com',
-        to: '',
-        cc: '',
-        title: '',
-        file: null,
-        description: '',
-        displayDescription: '',
-    };
     const [isShowMailMenu, setIsShowMailMenu] = useState(false);
     const [isEdit, setIsEdit] = useState(false);
-    const [ids, setIds] = useState<any>([]);
     const [searchText, setSearchText] = useState<any>('');
-    const [selectedMail, setSelectedMail] = useState<any>(null);
-    const [params, setParams] = useState<any>(JSON.parse(JSON.stringify(defaultParams)));
-    const [pagedMails, setPagedMails] = useState<any>([]);
     const isRtl = useSelector((state: IRootState) => state.themeConfig.rtlClass) === 'rtl' ? true : false;
     const [pager, setPager] = useState<any>({
         currentPage: 1,
@@ -85,31 +64,11 @@ const DashboardBox = () => {
     }, [loginuser?.client_user_id, dispatch]);
 
     useEffect(() => {
-        if (leads) {  
-            setAllLeadList(leads);
-         }
+        if (leads) {   setAllLeadList(leads); }
     }, [leads]);
 
 
-    
-
-
-    
-    
-
-
-
-    useEffect(() => {
-        const totalPages = Math.ceil(FilterLeadsList.length / pager.pageSize);
-        const startIndex = (pager.currentPage - 1) * pager.pageSize;
-        const endIndex = startIndex + pager.pageSize;
-        setPager((prev: any) => ({
-            ...prev,
-            totalPages,
-            startIndex,
-            endIndex: endIndex > FilterLeadsList.length ? FilterLeadsList.length : endIndex,
-        }));
-    }, [FilterLeadsList, pager.currentPage, pager.pageSize]);
+   
 
 
     const refreshMails = () => {
@@ -120,34 +79,7 @@ const DashboardBox = () => {
         if(selectSingleLead){ setSelectedLead(selectSingleLead); }
     }
 
-    const openMail = (type: string, item: any) => {
-        if (type === 'add') {
-            setIsShowMailMenu(false);
-            setParams(JSON.parse(JSON.stringify(defaultParams)));
-        } else if (type === 'draft') {
-            let data = JSON.parse(JSON.stringify(item));
-            setParams({ ...data, from: defaultParams.from, to: data.email, displayDescription: data.email });
-        } else if (type === 'reply') {
-            let data = JSON.parse(JSON.stringify(item));
-            setParams({
-                ...data,
-                from: defaultParams.from,
-                to: data.email,
-                title: 'Re: ' + data.title,
-                displayDescription: 'Re: ' + data.title,
-            });
-        } else if (type === 'forward') {
-            let data = JSON.parse(JSON.stringify(item));
-            setParams({
-                ...data,
-                from: defaultParams.from,
-                to: data.email,
-                title: 'Fwd: ' + data.title,
-                displayDescription: 'Fwd: ' + data.title,
-            });
-        }
-        setIsEdit(true);
-    };
+   
 
     const closeMsgPopUp = () => {
         setIsEdit(false);
@@ -198,35 +130,40 @@ const DashboardBox = () => {
         setFilterLeadsList(filterLead);
     };
 
+
+    useEffect(() => {
+        const totalPages = Math.ceil(FilterLeadsList.length / pager.pageSize);
+        const startIndex = (pager.currentPage - 1) * pager.pageSize;
+        const endIndex = Math.min(startIndex + pager.pageSize, FilterLeadsList.length);
+        setPager((prev:any) => ({
+            ...prev,
+            totalPages,
+            startIndex,
+            endIndex,
+        }));
+    }, [FilterLeadsList, pager.currentPage, pager.pageSize]);
+
+    const handlePageChange = (newPage: number) => {
+        if (newPage >= 1 && newPage <= pager.totalPages) {
+            setPager((prev:any) => ({
+                ...prev,
+                currentPage: newPage,
+            }));
+        }
+    };
+
     return (
         <div>
             <div className="flex gap-5 relative sm:h-[calc(100vh_-_150px)] h-full">
-                <div className={`overlay bg-black/60 z-[5] w-full h-full rounded-md absolute hidden ${isShowMailMenu ? '!block xl:!hidden' : ''}`}
-                    onClick={() => setIsShowMailMenu(!isShowMailMenu)}
+                <div className={`overlay bg-black/60 z-[5] w-full h-full rounded-md absolute hidden ${isShowMailMenu ? '!block xl:!hidden' : ''}`} onClick={() => setIsShowMailMenu(!isShowMailMenu)}
                 ></div>
-                <div className={`panel xl:block p-4 dark:gray-50 w-[250px] max-w-full flex-none space-y-3 xl:relative absolute z-10 xl:h-auto h-full hidden ltr:xl:rounded-r-md ltr:rounded-r-none rtl:xl:rounded-l-md rtl:rounded-l-none overflow-hidden ${
-                        isShowMailMenu ? '!block' : ''
-                    }`}
-                >
+                <div className={`panel xl:block p-4 dark:gray-50 w-[250px] max-w-full flex-none space-y-3 xl:relative absolute z-10 xl:h-auto h-full hidden ltr:xl:rounded-r-md ltr:rounded-r-none rtl:xl:rounded-l-md rtl:rounded-l-none overflow-hidden ${isShowMailMenu ? '!block' : '' }`}>
                     <div className="flex flex-col h-full pb-16">
-                        <div className="pb-5">
-                            <button className="btn btn-primary w-full" type="button" onClick={() => openMail('add', null)}> Add Lead </button>
+                        {/* onClick={() => openMail('add', null)} */}
+                        <div className="pb-5"> <button className="btn btn-primary w-full" type="button"> Add Lead </button>
                         </div>
                         <PerfectScrollbar className="relative ltr:pr-3.5 rtl:pl-3.5 ltr:-mr-3.5 rtl:-ml-3.5 h-full grow">
                             <div className="space-y-1">
-                                <button type="button" className={`w-full flex justify-between items-center p-2 hover:bg-white-dark/10 rounded-md dark:hover:text-primary hover:text-primary dark:hover:bg-[#181F32] font-medium h-10 ${
-                                        !isEdit && selectedTab === 'inbox' ? 'bg-gray-100 dark:text-primary text-primary dark:bg-[#181F32]' : ''
-                                    }`} onClick={() => { setSelectedTab('inbox');  }}>
-                                    <div className="flex items-center">
-                                        <IconMail className="w-5 h-5 shrink-0" />
-                                        <div className="ltr:ml-3 rtl:mr-3">Inbox</div>
-                                    </div>
-                                    <div className="bg-primary-light dark:bg-[#060818] rounded-md py-0.5 px-2 font-semibold whitespace-nowrap">
-                                        {/* {AllLeadList && AllLeadList.filter((d) => d.value == 2).length} */}
-                                        djddjdj
-                                    </div>
-                                </button>
-                                {/* sidebarstatus?.tab */}
                                 {
                                     SidebarStatuses.map((sidebarstatus) => (
                                         <button key={sidebarstatus?.value} type="button" className={`w-full flex justify-between items-center p-2 hover:bg-white-dark/10 rounded-md dark:hover:text-primary hover:text-primary dark:hover:bg-[#181F32] font-medium h-10`}  onClick={() => LeadsTabs(sidebarstatus?.value,)} >
@@ -250,17 +187,6 @@ const DashboardBox = () => {
                                 <div className="h-px border-b border-white-light dark:border-[#1b2e4b]"></div>
                             </div>
                         </PerfectScrollbar>
-                        <div className="ltr:left-0 rtl:right-0 absolute bottom-0 p-4 w-full">
-                            <button type="button" className="w-full flex p-2 justify-between items-center hover:bg-white-dark/10 rounded-md dark:hover:text-primary hover:text-primary dark:hover:bg-[#181F32] font-medium group" onClick={() => setIsShowMailMenu(false)} >
-                                <div className="flex items-center">
-                                    <IconUserPlus className="shrink-0" />
-                                    <div className="ltr:ml-3 rtl:mr-3">Add Account</div>
-                                </div>
-                                <div className="bg-primary-light dark:bg-[#060818] rounded-md py-1 px-2">
-                                    <IconPlus />
-                                </div>
-                            </button>
-                        </div>
                     </div>
                 </div>
                 <div className="panel p-0 flex-1 overflow-x-hidden h-full">
@@ -283,7 +209,6 @@ const DashboardBox = () => {
                                         </button>
                                         <div className="relative group">
                                             <input type="text" className="form-input ltr:pr-8 rtl:pl-8 peer" placeholder="Search Mail" value={searchText} onChange={(e) => setSearchText(e.target.value)} />
-                                            {/* onKeyUp={() => searchMails()} */}
                                             <div className="absolute ltr:right-[11px] rtl:left-[11px] top-1/2 -translate-y-1/2 peer-focus:text-primary">
                                                 <IconSearch />
                                             </div>
@@ -294,30 +219,18 @@ const DashboardBox = () => {
                             <div className="h-px border-b border-white-light dark:border-[#1b2e4b]"></div>
                             <div className="flex flex-wrap flex-col md:flex-row xl:w-auto justify-between items-center px-4 pb-4">
                                 <div className="w-full sm:w-auto grid grid-cols-4 sm:grid-cols-7 gap-1 mt-4">
-                                    {/* status.tab */}
                                     {TopbarStatuses.map((status) => (
                                         <button  key={status.value} onClick={() => LeadsTabs(status.value)}  type="button"  className={`btn ${status.outlineColor} flex ${selectedTab === status.label}`}> {status.icon} {status.label}  </button>
                                     ))}
-                   
                                 </div>
                                 <div className="mt-4 md:flex-auto flex-1">
                                     <div className="flex items-center md:justify-end justify-center">
-                                        <div className="ltr:mr-3 rtl:ml-3">10</div>
-                                        {/* {pager.startIndex + 1 + '-' + (pager.endIndex + 1) + ' of ' + filteredMailList.length} */}
-                                        <button
-                                            type="button"
-                                            disabled={pager.currentPage === 1}
+                                        <div className="ltr:mr-3 rtl:ml-3"> {pager.startIndex + 1 + '-' + (pager.endIndex) + ' of ' + FilterLeadsList.length} </div>
+                                        <button onClick={() => handlePageChange(pager.currentPage - 1)} type="button" disabled={pager.currentPage === 1}
                                             className="bg-[#f4f4f4] rounded-md p-1 enabled:hover:bg-primary-light dark:bg-white-dark/20 enabled:dark:hover:bg-white-dark/30 ltr:mr-3 rtl:ml-3 disabled:opacity-60 disabled:cursor-not-allowed">
                                             <IconCaretDown className="w-5 h-5 rtl:-rotate-90 rotate-90" />
-                                            {/* onClick={() => { pager.currentPage--; searchMails(false); }} */}
                                         </button>
-                                        <button
-                                            type="button"
-                                            disabled={pager.currentPage === pager.totalPages}
-
-                                            className="bg-[#f4f4f4] rounded-md p-1 enabled:hover:bg-primary-light dark:bg-white-dark/20 enabled:dark:hover:bg-white-dark/30 disabled:opacity-60 disabled:cursor-not-allowed">
-                                            <IconCaretDown className="w-5 h-5 rtl:rotate-90 -rotate-90" />
-                                            {/* onClick={() => { pager.currentPage++; searchMails(false); }}  */}
+                                        <button onClick={() => handlePageChange(pager.currentPage + 1)} type="button" disabled={pager.currentPage === pager.totalPages} className="bg-[#f4f4f4] rounded-md p-1 enabled:hover:bg-primary-light dark:bg-white-dark/20 enabled:dark:hover:bg-white-dark/30 disabled:opacity-60 disabled:cursor-not-allowed"> <IconCaretDown className="w-5 h-5 rtl:rotate-90 -rotate-90" />
                                         </button>
                                     </div>
                                 </div>
@@ -327,7 +240,7 @@ const DashboardBox = () => {
                                     <div className="table-responsive grow overflow-y-auto sm:min-h-[300px] min-h-[400px]">
                                         <table className="table-hover">
                                             <tbody>
-                                                {FilterLeadsList.map((lead: any) => {
+                                              { FilterLeadsList.slice(pager.startIndex, pager.endIndex).map((lead: any) => {
                                                     return (
                                                         <tr key={lead.id} className="cursor-pointer" onClick={() => selectLead(lead)}>
                                                             <td>
@@ -339,7 +252,6 @@ const DashboardBox = () => {
                                                                                 <IconBookmark bookmark={false} className={`w-4.5 h-4.5 ${lead.isImportant && 'fill-primary'}`} />
                                                                             </button>
                                                                         </Tippy>
-                                                                        {/* onClick={(e) => { e.stopPropagation(); setImportant(lead.id); }} */}
                                                                     </div>
                                                                     <div className={`dark:text-gray-300 whitespace-nowrap font-semibold ${ !lead.isUnread ? 'text-gray-500 dark:text-gray-500 font-normal' : ''}`}
                                                                     > {lead?.lead_title || 'Not Found'}
@@ -535,32 +447,13 @@ const DashboardBox = () => {
                             <div className="h-px bg-gradient-to-l from-indigo-900/20 via-black dark:via-white to-indigo-900/20 opacity-[0.1]"></div>
                             <form className="p-6 grid gap-6">
                                 <div>
-                                    <input id="to" type="text" className="form-input" placeholder="Enter To" defaultValue={params.to} />
-                                    {/* onChange={(e) => { changeValue(e); }} */}
+                                    <input id="to" type="text" className="form-input" placeholder="Enter To" />
                                 </div>
                                 <div>
-                                    {/* onChange={(e) => changeValue(e)} */}
-                                    <input id="cc" type="text" className="form-input" placeholder="Enter Cc" defaultValue={params.cc} />
+                                    <input id="cc" type="text" className="form-input" placeholder="Enter Cc"  />
                                 </div>
                                 <div>
-                                    {/* onChange={(e) => changeValue(e)} */}
-                                    <input id="title" type="text" className="form-input" placeholder="Enter Subject" defaultValue={params.title} />
-                                </div>
-
-                                <div className="h-fit">
-                                    <ReactQuill
-                                        theme="snow"
-                                        value={params.description || ''}
-                                        defaultValue={params.description || ''}
-                                        onChange={(content, delta, source, editor) => {
-                                            params.description = content;
-                                            params.displayDescription = editor.getText();
-                                            setParams({
-                                                ...params,
-                                            });
-                                        }}
-                                        style={{ minHeight: '200px' }}
-                                    />
+                                    <input id="title" type="text" className="form-input" placeholder="Enter Subject" />
                                 </div>
                                 <div>
                                     <input
@@ -575,11 +468,9 @@ const DashboardBox = () => {
                                     <button type="button" className="btn btn-outline-danger ltr:mr-3 rtl:ml-3" onClick={closeMsgPopUp}>
                                         Close
                                     </button>
-                                    {/* onClick={() => saveMail('save', null)} */}
                                     <button type="button" className="btn btn-success ltr:mr-3 rtl:ml-3">
                                         Save
                                     </button>
-                                    {/* onClick={() => saveMail('send', params.id)} */}
                                     <button type="button" className="btn btn-primary">
                                         Send
                                     </button>
