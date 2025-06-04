40027822fc443c13de1bdb7b70681647ed9872be
idrees-office <idreesoffice9012@gmail.com>
2025-06-04 10:49:11 +0400

    update base url

---
+++ dist/assets/Error-91e60d95.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-d50841d6.js";const e=()=>r("div",{children:r("h1",{children:"Error"})});export{e as default};
--- src/components/BaseUrl.tsx
+++ src/components/BaseUrl.tsx
@@ -1,14 +1,12 @@
 export const getBaseUrl = () => {
-    const currentUrl = window.location.href;
-    if (currentUrl.includes('localhost')) {
-        // return 'https://newcrmbackend.ddev.site/api'; 
-        return 'http://10.99.1.119:8000/api';
-        return 'http://10.99.1.99:8000/api';
-    } else if (currentUrl.includes('leadshub.ae')) {
-        return 'https://backend.leadshub.ae/api';  
-     } else if (currentUrl.includes('testcrm.leadshub.ae')) {
-        return 'https://testcrmbackend.leadshub.ae/api';
-    } else {
-        return 'http://10.99.1.93:8000/api';   
-    }
+  const currentUrl = window.location.href;
+  if (currentUrl.includes('localhost')) {
+    return 'http://10.99.1.119:8000/api';
+  } else if (currentUrl.includes('testcrm.leadshub.ae')) {
+    return 'https://testcrmbackend.leadshub.ae/api';
+  } else if (currentUrl.includes('leadshub.ae')) {
+    return 'https://backend.leadshub.ae/api';
+  } else {
+    return 'http://10.99.1.93:8000/api';
+  }
 };
