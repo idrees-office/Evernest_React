b6617ddf59e9bf48ab21ac41aac1f3723ae7f7ac
Marsad1998 <marsadakbar1@gmail.com>
2025-07-04 14:23:47 +0500

    fix the pdf

--- src/main.tsx
+++ src/main.tsx
@@ -5,11 +5,8 @@ import './tailwind.css';
 import './i18n';
 import { RouterProvider } from 'react-router-dom';
 import router from './router/index';
-
 import { Provider } from 'react-redux';
 import store from './store';
-
-
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
