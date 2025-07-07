99d77e2e78f6b01b99b538fc3e8d014702aea867
Marsad1998 <marsadakbar1@gmail.com>
2025-07-07 14:10:34 +0500

    fix the pdf

--- src/components/BaseUrl.tsx
+++ src/components/BaseUrl.tsx
@@ -1,13 +1,8 @@
 export const getBaseUrl = () => {
   const currentUrl = window.location.href;
   if (currentUrl.includes('localhost')) {
-<<<<<<< HEAD
     return 'https://newcrmbackend.ddev.site/api';
     return 'http://10.99.1.45:8000/api';
-=======
-    // return 'https://newcrmbackend.ddev.site/api';
-    return 'http://10.99.1.53:8000/api';
->>>>>>> 56d7c59a79c60929cfb41dabcb8d09ea243ae044
   } else if (currentUrl.includes('testcrm.leadshub.ae')) {
     return 'https://testcrmbackend.leadshub.ae/api';
   } else if (currentUrl.includes('leadshub.ae')) {
