4e872f9b24a9f7ec53464f266642d5dedf3636d5
Marsad1998 <marsadakbar1@gmail.com>
2025-07-07 15:17:02 +0500

    fix the pdf1

--- dist/assets/Error-a55d53a1.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-933fe52f.js";const e=()=>r("div",{children:r("h1",{children:"Error"})});export{e as default};
--- dist/assets/IconPlus-5b45b178.js
+++
@@ -1,5 +0,0 @@
-<<<<<<<< HEAD:dist/assets/IconPlus-5b45b178.js
-import{a as r,j as e}from"./index-7458b33d.js";const i=({className:o,fill:n=!1,duotone:s=!0})=>r("svg",{width:"20",height:"20",viewBox:"0 0 24 24",stroke:"currentColor",strokeWidth:"1.5",fill:"none",strokeLinecap:"round",strokeLinejoin:"round",className:o,children:[e("line",{x1:"12",y1:"5",x2:"12",y2:"19"}),e("line",{x1:"5",y1:"12",x2:"19",y2:"12"})]});export{i as I};
-========
-import{a as r,j as e}from"./index-933fe52f.js";const i=({className:o,fill:n=!1,duotone:s=!0})=>r("svg",{width:"20",height:"20",viewBox:"0 0 24 24",stroke:"currentColor",strokeWidth:"1.5",fill:"none",strokeLinecap:"round",strokeLinejoin:"round",className:o,children:[e("line",{x1:"12",y1:"5",x2:"12",y2:"19"}),e("line",{x1:"5",y1:"12",x2:"19",y2:"12"})]});export{i as I};
->>>>>>>> 99d77e2e78f6b01b99b538fc3e8d014702aea867:dist/assets/IconPlus-2be99c55.js
